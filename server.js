d69e99ac1036fdd470e3512f35b4b73c024677c5
abood <abdulrahman.mater.08@gmail.com>
2026-07-17 19:32:29 +0300

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 
+const tasks = [
+  {
+    id:1,
+    title:"Study",
+    done:true
+  },
+  {
+    id:2,
+    title:"apply",
+    done:false
+  },
+  {
+    id:3,
+    title:"play",
+    done:false
+  }
+]
+
+
 app.get("/", (req, res) => {
     res.status(200).json({
         name: "Task API",
@@ -17,6 +36,28 @@ app.get("/health", (req, res) => {
     })
 })
 
+app.get("/tasks",(req, res)=>{
+    if (tasks.length===0){
+        return res.json({
+            message:"There are no tasks"
+        })
+    }
+    res.json(tasks)
+})
+
+app.get("/tasks/:id",(req, res)=>{
+    const id = Number(req.params.id);
+    const task = tasks.find((task)=>{
+        return task.id === id
+    })
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    res.json(task);
+})
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}.....`)
