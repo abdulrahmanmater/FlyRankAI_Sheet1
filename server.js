7c240e8714f99130e0d0ac8cf2fb2b8f8b8f68f2
abood <abdulrahman.mater.08@gmail.com>
2026-07-17 18:38:38 +0300

    Stage 0: hello server

--- server.js
+++ server.js
@@ -3,20 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    console.log("Hello world from home page")
-    res.status(200).json({
-        message: "Hello to my API from home page",
-        success: true
-    })
-})
-app.get("/flyRankAI", (req, res) => {
-    console.log("Hello world from FlyRank AI")
-    res.status(200).json({
-        message: "Hello to my API from FlyRank AI",
-        success: true
-    })
-})
+
+
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}.....`)
